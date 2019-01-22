707fe982cf79b0c9fd0f4d0a26a6c9954bc60f66
Alban Mouton <alban.mouton@gmail.com>
2019-01-22 21:41:06 +0100

    fix: add quotes to support spaces in filters

--- assets/filters-utils.js
+++ assets/filters-utils.js
@@ -2,11 +2,11 @@ export function filter2qs(filter) {
   if (!filter.type || filter.type === 'in') {
     if ([null, undefined, ''].includes(filter.values)) return null
     if (Array.isArray(filter.values) && filter.values.length === 0) return null
-    return filter.values.map(v => `${filter.field.key}:${v}`).join(' OR ')
+    return filter.values.map(v => `${filter.field.key}:"${v}"`).join(' OR ')
   } else if (filter.type === 'out') {
     if ([null, undefined, ''].includes(filter.values)) return null
     if (Array.isArray(filter.values) && filter.values.length === 0) return null
-    return filter.values.map(v => `!(${filter.field.key}:${v})`).join(' AND ')
+    return filter.values.map(v => `!(${filter.field.key}:"${v})"`).join(' AND ')
   } else if (filter.type === 'interval') {
     if (!filter.minValue || !filter.maxValue) return null
     return `${filter.field.key}:[${filter.minValue} TO ${filter.maxValue}]`
