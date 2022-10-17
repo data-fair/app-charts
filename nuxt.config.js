76e34d474f75e0dcc031a39d5ae9eccdda69f2bc
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2022-10-17 17:38:58 +0200

    feat: meta for captures

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'df:overflow', content: 'false' },
+      { name: 'df:sync-state', content: 'false' },
       { hid: 'version', name: 'version', content: process.env.NODE_ENV === 'development' ? 'development' : pjson.version },
       { hid: 'application', name: 'application-name', content: 'Charts' },
       { hid: 'description', name: 'description', content: 'Une application simple de graphiques pour data-fair.' }
