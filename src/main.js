577eb49279944e9a8ecf874e2abf3e10cc210cea
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2024-10-14 16:50:39 +0200

    fix: vuetify init for colors

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import 'vuetify/styles'
 import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
 import { defaultOptions } from '@data-fair/lib/vuetify.js'
+import reactiveSearchParams from '@data-fair/lib/vue/reactive-search-params-global.js'
 import App from './App.vue'
 
 const app = createApp(App)
 
 // @ts-ignore
-app.use(createVuetify(defaultOptions))
+app.use(createVuetify(defaultOptions(reactiveSearchParams)))
 
 app.mount('#app')
